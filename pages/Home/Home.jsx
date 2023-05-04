020ecd18eaf20c927ad191f4a260277b61e1bb42
lrag14 <laurentrober@gmail.com>
2023-05-04 12:37:15 +0200

    create folder

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -3,10 +3,7 @@ import { useParams } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      {" "}
       <header>
-        <img alt="vinted Logo" src="./public/logo_vinted.png" />
-
         <button>s'incrire</button>
         <button>se connecter</button>
         <button>vends tes articles</button>
